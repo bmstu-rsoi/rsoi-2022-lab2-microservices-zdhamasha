d560800616fbbea643e45f7ea9d391c3d1eedc83
Zaid Hamasha <zaid.hamasha@progressoft.com>
2022-11-05 13:55:35 +0300

    chore: implement flight controller

--- ticket/src/main/java/rsoi/lab2/ticket/controller/TicketController.java
+++ ticket/src/main/java/rsoi/lab2/ticket/controller/TicketController.java
@@ -1,6 +1,7 @@
 package rsoi.lab2.ticket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rsoi.lab2.ticket.repoistory.TicketRepoistory;
@@ -10,4 +11,5 @@ import rsoi.lab2.ticket.repoistory.TicketRepoistory;
 public class TicketController {
     @Autowired
     private TicketRepoistory ticketRepoistory;
+
 }
--- ticket/src/main/java/rsoi/lab2/ticket/repoistory/TicketRepoistory.java
+++ ticket/src/main/java/rsoi/lab2/ticket/repoistory/TicketRepoistory.java
@@ -2,6 +2,6 @@ package rsoi.lab2.ticket.repoistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rsoi.lab2.ticket.model.Ticket;
 
-public interface TicketRepoistory extends JpaRepository<Ticket, Integer> {
+public interface TicketRepoistory extends JpaRepository<Ticket, Long> {
 
 }
